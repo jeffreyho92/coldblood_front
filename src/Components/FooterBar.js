bdc6bdc62029b6b206668f6bc104e2c173999a6e
jeffrey <jeffreyho92@gmail.com>
2018-05-25 21:45:59 +0800

    update

--- src/Components/FooterBar.js
+++ src/Components/FooterBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, Button } from "react-bootstrap";
 import "./FooterBar.css";
+import fb_me from "../img/facebook-messenger.png";
+import icon_fb from "../img/icon-fb.svg";
+import icon_insta from "../img/icon-insta.svg";
 
 class FooterBar extends Component {
   render() {
@@ -9,7 +12,25 @@ class FooterBar extends Component {
         <Grid>
           <Row>
             <Col md={12}>
-              <div style={{ height: 100, paddingTop: 100, paddingBottom: 50 }}>© All images are copyrighted by their respective authors.</div>
+              <div style={{ height: 100, paddingTop: 50, paddingBottom: 60 }}>
+                <a href="https://www.facebook.com/coldbloodapp/" target="_blank">
+                  <img src={icon_fb} className="social_icon" />
+                </a>
+                &emsp;
+                <a href="https://www.instagram.com/app.coldblood/" target="_blank">
+                  <img src={icon_insta} className="social_icon" />
+                </a>
+                &emsp;
+                <a href="https://m.me/635728603438569" target="_blank">
+                  <Button className="btn_fb_me">
+                    <img src={fb_me} />
+                    &nbsp; Message Us
+                  </Button>
+                </a>
+                <br />
+                <br />
+                © All images are copyrighted by their respective authors.
+              </div>
             </Col>
           </Row>
         </Grid>
